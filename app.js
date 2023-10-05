2a991309b66f99bcc25b9734dee523678031adad
bushraaaaaaaaa <bushraqadir78@gmail.com>
2023-10-06 00:40:34 +0500

    two files added

---
+++ app.js
@@ -0,0 +1,134 @@
+// q1
+// var date=new Date();
+// console.log(date);
+// q2
+// var a=prompt("enter your first name");
+// var b=prompt("enter your last name");   
+// function greet(a,b){
+//     return (a + " " + b);
+// }
+// var res=greet(a,b);
+// console.log(res);
+// q3
+// var a=+prompt("enter your first number");
+// var b=+prompt("enter your last number");   
+// function sum(a,b){
+//     return (a+b);
+// }
+// var res=sum(a,b);
+// console.log(res);
+// q4
+// var a=+prompt("enter your first number");
+// var b=+prompt("enter your last number");
+// var opr=prompt("enter operator")   
+// function calculator(a,b,opr){
+//     if(opr==="+"){
+//         return a+b;
+//     }
+//     else if(opr==="-"){
+//         return a-b;
+//     }
+//     else if(opr==="*"){
+//         return a*b;
+//     }
+//     else if(opr==="/"){
+//         return a/b;
+//     }
+//     else{
+//         console.log("wrong operator")
+//     }
+// }
+// var res=calculator(a,b,opr);
+// console.log(res);
+// q5
+// var a=+prompt("enter number you want to square");
+// function square(a){
+//     return a*a;
+// }  
+// q6
+// var res=square(a);
+// console.log(res);
+// q7
+// var a=+prompt("enter first number");
+// var b=+prompt("enter last number");
+// function count(a,b){
+//     for(var i=a;i<=b;i++){
+//         console.log(i);
+//     }
+// }
+// count(a,b);
+// q8
+// var a=+prompt("enter base number");
+// var b=+prompt("enter prep number");
+// function hyp(a,b){
+//     return Math.sqrt(a*a + b*b);
+// }
+// var res=hyp(a,b);
+// console.log(res);
+// q9
+// b-
+// var width=+prompt("enter width");
+// var height=+prompt("enter height");
+// function area(width,height){
+//     return width*height;
+// }
+// var res=area(width,height);
+// console.log(res);
+// a-
+// function area(width,height){
+//     return width*height;
+// }
+// var res=area(4,5);
+// console.log(res);
+// q10
+// var word=prompt("enter a word to check palindrome");
+// var change="";
+// function palindrome(word,change){
+//     for(var i=word.length-1;i>=0;i--){
+// change=change+word[i]
+//     }
+//     if(word==change){
+//         console.log("it is palindrome");
+//     }
+//     else{
+//         console.log("not a palindrome")
+//     }
+// }
+// palindrome(word,change);
+// q11
+// function string(){
+//     var sentence="the quick brown fox";
+//     var firstword=sentence.slice(0,1).toUpperCase();
+//     var remain=sentence.slice(1).toLowerCase();
+// var combine=firstword+remain;
+// // var index=sentence.indexOf("q");
+// // console.log(index);
+// console.log(combine);
+// }
+// string();
+// q13
+// function occ(){
+//     var sentence="JSResourceS.com"
+//     var count=0;
+//     for(var i=0;i<=sentence.length;i++){
+//     if(sentence[i]==="o"){
+//         count++;
+//     }
+// }
+//     console.log(count);
+// }
+// occ();
+// q14
+// var a=prompt("enter the radius value of circumference");
+// var b=prompt("enter the radius value of area");
+// function calcCircumference(a){
+//     return 3.142*2*a;
+// }
+// var result=calcCircumference(a);
+// console.log("the circumference is " + result);
+// function calcarea(b){
+//     return 3.142*b*b;
+// }
+// var res=calcarea(b);
+// console.log("the area of circle  is " + res);
+
